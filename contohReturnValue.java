443e680340476865e48261a9dd7a330870878cff
gospel <2381043@unai.edu>
2024-09-20 08:22:25 +0700

    handson 4

---
+++ contohReturnValue.java
@@ -0,0 +1,15 @@
+public class contohReturnValue {
+        public static void sebutkanNama() {
+            System.out.println("Nama Saya Gospel");
+        }
+
+        public static int kembalikanAngka() {
+            int hasilPerkalian = 20 * 3;
+            return hasilPerkalian;
+        }
+
+        public static void main(String[] args) {
+
+        }
+    }
+
